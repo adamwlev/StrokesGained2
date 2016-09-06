03962162483d93383803ce40e2eea1357fb781b3
adamwlev <awl44@me.com>
2016-09-06 11:20:22 -0400

    9

--- makeStrokesGainedBaseline.py
+++ makeStrokesGainedBaseline.py
@@ -1,110 +1,69 @@
 import pandas as pd
 import numpy as np
 import itertools
+from math import atan2,radians
 from sklearn.isotonic import IsotonicRegression
-from sklearn.linear_model import LinearRegression
 import multiprocessing
 
-def convert_cats(cat,dist):
+def convert_cats(cat,dist,shot):
     if cat in ['Green Side Bunker','Fairway Bunker']:
         return 'Bunker'
     elif cat not in ['Green','Fairway','Fringe','Primary Rough','Intermediate Rough','Tee Box']:
         return 'Other'
     elif cat=='Fringe' and dist>120:
         return 'Intermediate Rough'
+    elif cat=='Tee Box' and shot!=1:
+    	return 'Fairway'
     else:
         return cat
-
-def run_a_slice(slice):
-	little_dict = {}
-	for crhytup in slice:
-	    subset = data[np.where((data[:,0]==crhytup[0]) & (data[:,1]==crhytup[1]) & (data[:,2]==crhytup[2]) & (data[:,13]==crhytup[3]))]
-	    if subset.shape[0]==0:
-	        continue
-	    players = pd.unique(subset[:,3])
-	    if len(players)<=1:
-	        continue
-	    scores = {player:int(subset[np.where(subset[:,3]==player)][0,4]) for player in players}
-	    ave_score = np.mean(np.array([scores.get(player) for player in players]))
-	    for player in players:
-	        sub = subset[np.where(subset[:,3]!=player)]
-	        #print len(sub)
-	        models = {}
-	        ranges_covered = {}
-	        for cat in cats:
-	            if len(sub[np.where(sub[:,6]==cat)])>6:
-	                models[cat] = LinearRegression()
-	                models[cat].fit(sub[np.where(sub[:,6]==cat)][:,8][:,None],sub[np.where(sub[:,6]==cat)][:,7])
-	                ranges_covered[cat] = (np.amin(sub[np.where(sub[:,6]==cat)][:,8]),np.amax(sub[np.where(sub[:,6]==cat)][:,8]))
-
-	        just_dist_model = LinearRegression()
-	        just_dist_model.fit(sub[:,8][:,None],sub[:,7])
-
-	        sub = subset[np.where(subset[:,3]==player)]
-	        
-	        if scores[player]!=sub.shape[0]:
-	            print 'hmmm'
-
-	        for row_ind in range(2,scores[player]+1):
-	            shot = sub[np.where(sub[:,5]==row_ind)]
-	            cat = shot[0,6]
-	            dist = shot[0,8]
-	            shot_before = sub[np.where(sub[:,5]==row_ind-1)]
-	            cat_before = shot_before[0,6]
-	            dist_before = shot_before[0,8]
-	            if row_ind==2:
-	                if cat not in models or dist<ranges_covered[cat][0] or dist>ranges_covered[cat][1]:
-	                    normal_cat_diff = overall_models[cat].predict([dist])[0] - overall_just_dist.predict([dist])[0]
-	                    difficulty_end = just_dist_model.predict(np.array([dist])[:,None])[0] + normal_cat_diff
-	                else:
-	                    difficulty_end = models[cat].predict(np.array([dist])[:,None])[0]
-	            	little_dict[crhytup+(player,row_ind-1)] = ave_score - difficulty_end - 1
-	            else:
-	                if cat not in models or dist<ranges_covered[cat][0] or dist>ranges_covered[cat][1]:
-	                    normal_cat_diff = overall_models[cat].predict([dist])[0] - overall_just_dist.predict([dist])[0]
-	                    difficulty_end = just_dist_model.predict(np.array([dist])[:,None])[0] + normal_cat_diff
-	                else:
-	                    difficulty_end = models[cat].predict(np.array([dist])[:,None])[0]
-	                if cat_before not in models or dist_before<ranges_covered[cat_before][0] or dist_before>ranges_covered[cat_before][1]:
-	                    normal_cat_diff = overall_models[cat_before].predict([dist_before])[0] - overall_just_dist.predict([dist_before])[0]
-	                    difficulty_start = just_dist_model.predict(np.array([dist_before])[:,None])[0] + normal_cat_diff
-	                else:
-	                    difficulty_start = models[cat_before].predict(np.array([dist_before])[:,None])[0]
-	                
-	            	little_dict[crhytup+(player,row_ind-1)] = difficulty_start - difficulty_end - 1
-
-	        cat_last = sub[np.where(sub[:,5]==scores[player])][0,6]
-	        dist_last = sub[np.where(sub[:,5]==scores[player])][0,8]
-	        if cat_last not in models or dist_last<ranges_covered[cat_last][0] or dist_last>ranges_covered[cat_last][1]:
-	            normal_cat_diff = overall_models[cat_last].predict([dist_last])[0] - overall_just_dist.predict([dist_last])[0]
-	            difficulty_last = just_dist_model.predict(np.array([dist_last])[:,None])[0] + normal_cat_diff
-	        else:
-	            difficulty_last = models[cat_last].predict(np.array([dist_last])[:,None])[0]
-	        little_dict[crhytup+(player,scores[player])] = difficulty_last - 1
-	return little_dict
 
 def partition (lst, n):
 	return [lst[i::n] for i in xrange(n)]
 
-for YEAR in range(2003,2005):
+def get_green_to_work_with(df,points,slack):
+    df.insert(len(df.columns),'angle',[atan2(y,x) for x,y in zip(df.Started_at_X,
+                                                                 df.Started_at_Y)])
+    ww = []
+    def get_sub(df,angle,slack):
+        return df[(df.angle>angle-slack) & (df.angle<angle+slack)]
+    for point in points:
+        angle = atan2(point[1],point[0])
+        sub = get_sub(df,angle,slack)
+        c = 0
+        while len(sub)==0:
+            c += 1
+            if c==15:
+                break
+            sub = get_sub(df,angle,slack+2)
+        if c==15:
+            ww.append(np.nan)
+        else:
+            ww.append(sub.Distance_from_hole.max())
+    return ww
+
+def run_a_slice(slice):
+	little_dict = {}
+	for course,round,hole,year in slice:
+	    subset = data[(data['Course_#']==course) & (data.Round==round) & (data.Hole==hole) & (data.Year==year) & (data.Shot!=1)]
+	    if subset.shape[0]==0:
+	        continue
+        green = subset[subset.Cat=='Green']
+        if len(green)<5:
+            continue
+        non_green = subset[subset.Cat!='Green'] 
+		to_work_with = get_green_to_work_with(green,zip(non_green.Started_at_X,non_green.Started_at_Y),slack=radians(25))
+		little_dict[(course,round,hole,year)] = {(pl,sh):ww for pl,sh,ww in zip(non_green['Player_#'],non_green.Shot,to_work_with)}
+	return little_dict
+
+for YEAR in range(2016,2017):
+	print YEAR
 	data = pd.read_csv('data/%d.csv' % YEAR)
 
-	data.insert(len(data.columns),'Cat',[convert_cats(c,d) for c,d in zip(data['From_Location(Scorer)'],data['Distance_from_hole'])])
+	data.insert(len(data.columns),'Cat',[convert_cats(c,d) for c,d in zip(data['From_Location(Scorer)'],data['Distance_from_hole'],data.Shot)])
 
 	uCRHYtps = list(itertools.product(pd.unique(data['Course_#']),pd.unique(data.Round),pd.unique(data.Hole),pd.unique(data.Year)))
 
-	data = data[['Course_#','Round','Hole','Player_#','Hole_Score','Shot','Cat','Shots_taken_from_location',
-	            'Distance_from_hole','Started_at_X','Started_at_Y','Went_to_X','Went_to_Y','Year']].values
-
 	cats = ['Bunker','Other','Green','Fairway','Fringe','Primary Rough','Intermediate Rough']
-
-	overall_models = {}
-	for cat in cats:
-	    overall_models[cat] = IsotonicRegression(out_of_bounds='clip')
-	    overall_models[cat].fit(data[np.where(data[:,6]==cat)][:,8],data[np.where(data[:,6]==cat)][:,7])
-
-	overall_just_dist = IsotonicRegression(out_of_bounds='clip')
-	overall_just_dist.fit(data[:,8],data[:,7])
 
 	num_cores = multiprocessing.cpu_count()
 	slices = partition(uCRHYtps,num_cores)
@@ -113,8 +72,8 @@ for YEAR in range(2003,2005):
 
 	big_dict = {key:value for little_dict in results for key,value in little_dict.iteritems()}
 	cols = ['Course_#','Round','Hole','Year','Player_#','Shot']
-	data = pd.read_csv('data/%d.csv' % YEAR)
-	print len(big_dict),len(data)
-	data['Strokes_Gained'] = [big_dict[tuple(tup)] if tuple(tup) in big_dict else np.nan for tup in data[cols].values.astype(int).tolist()]
-
-	data.to_csv('data/%d.csv' % YEAR,index=False)
+	ww = [big_dict[(course,round,hole,year)][(player,shot)] if (course,round,hole,year) in big_dict and (player_shot) in big_dict[(course,round,hole,year)] else np.nan for course,round,hole,year,player,shot in data[cols].values.tolist()]
+	data.insert(len(data.columns),'Green_to_work_with',ww)
+	print data[(data.Shot!=1) && (data.Cat!='Green')].Green_to_work_with.describe()
+	#data['Strokes_Gained'] = [big_dict[tuple(tup)] if tuple(tup) in big_dict else np.nan for tup in data[cols].values.astype(int).tolist()]
+	#data.to_csv('data/%d.csv' % YEAR,index=False)
