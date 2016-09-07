e336375e71c6ba9ba2b38827af078f475df3c068
adamwlev <awl44@me.com>
2016-09-07 17:28:18 -0400

    save the cats

--- SaveTheCats.py
+++ SaveTheCats.py
@@ -1,74 +1,99 @@
 import pandas as pd
 import numpy as np
+import itertools
+import math
 from scipy.sparse import csc_matrix
-import os
-from scipy.spatial.distance import pdist
+from scipy.spatial.distance import pdist,squareform
+import os,sys
+import multiprocessing
+    
+if __name__=='__main__':
+    cats = {}
+    cats['green0'] = 'Cat=="Green" & Distance_from_hole<5'
+    cats['fringe0'] = 'Cat=="Fringe" & Distance_from_hole<5'
+    cats['green5'] = 'Cat=="Green" & Distance_from_hole>=5 & Distance_from_hole<10'
+    cats['fringe5'] = 'Cat=="Fringe" & Distance_from_hole>=5 & Distance_from_hole<10'
+    cats['green10'] = 'Cat=="Green" & Distance_from_hole>=10 & Distance_from_hole<20'
+    cats['fringe10'] = 'Cat=="Fringe" & Distance_from_hole>=10 & Distance_from_hole<20'
+    cats['green20'] = 'Cat=="Green" & Distance_from_hole>=20'
+    cats['fringe20'] = 'Cat=="Fringe" & Distance_from_hole>=20'
+    cats['prough0'] = 'Cat=="Primary Rough" & Distance_from_hole<90'
+    cats['irough0'] = 'Cat=="Intermediate Rough" & Distance_from_hole<90'
+    cats['prough90'] = 'Cat=="Primary Rough" & Distance_from_hole>=90 & Distance_from_hole<375'
+    cats['irough90'] = 'Cat=="Intermediate Rough" & Distance_from_hole>=90 & Distance_from_hole<375'
+    cats['prough375'] = 'Cat=="Primary Rough" & Distance_from_hole>=375'
+    cats['irough375'] = 'Cat=="Intermediate Rough" & Distance_from_hole>=375'
+    cats['fairway0'] = 'Cat=="Fairway" & Distance_from_hole<300'
+    cats['fairway300'] = 'Cat=="Fairway" & Distance_from_hole>=300 & Distance_from_hole<540'
+    cats['fairway540'] = 'Cat=="Fairway" & Distance_from_hole>=540'
+    cats['bunker'] = 'Cat=="Bunker"'
+    cats['tee3'] = 'Cat=="Tee Box" & Par_Value==3'
+    cats['tee45'] = 'Cat=="Tee Box" & (Par_Value==4 | Par_Value==5)'
 
-cats = {}
-cats['green0'] = ['Cat=="Green" & Distance_from_hole<5','Cat=="Fringe" & Distance_from_hole<5']
-cats['green5'] = ['Cat=="Green" & Distance_from_hole>=5 & Distance_from_hole<10','Cat=="Fringe" & Distance_from_hole>=5 & Distance_from_hole<10']
-cats['green10'] = ['Cat=="Green" & Distance_from_hole>=10 & Distance_from_hole<20','Cat=="Fringe" & Distance_from_hole>=10 & Distance_from_hole<20']
-cats['green20'] = ['Cat=="Green" & Distance_from_hole>=20','Cat=="Fringe" & Distance_from_hole>=20']
-cats['rough0'] = ['Cat=="Primary Rough" & Distance_from_hole<90','Cat=="Intermediate Rough" & Distance_from_hole<90']
-cats['rough90'] = ['Cat=="Primary Rough" & Distance_from_hole>=90 & Distance_from_hole<375','Cat=="Intermediate Rough" & Distance_from_hole>=90 & Distance_from_hole<375']
-cats['rough375'] = ['Cat=="Primary Rough" & Distance_from_hole>=375','Cat=="Intermediate Rough" & Distance_from_hole>=375']
-cats['fairway0'] = ['Cat=="Fairway" & Distance_from_hole<300']
-cats['fairway300'] = ['Cat=="Fairway" & Distance_from_hole>=300 & Distance_from_hole<540']
-cats['fairway540'] = ['Cat=="Fairway" & Distance_from_hole>=540']
-cats['bunker'] = ['Cat=="Bunker"']
-cats['tee3'] = ['Cat=="Tee Box" & Par_Value==3']
-cats['tee45'] = ['Cat=="Tee Box" & (Par_Value==4 | Par_Value==5)']
+    def convert_cats(cat,dist,shot):
+        if cat in ['Green Side Bunker','Fairway Bunker']:
+            return 'Bunker'
+        elif cat not in ['Green','Fairway','Fringe','Primary Rough','Intermediate Rough','Tee Box']:
+            return 'Other'
+        elif cat=='Fringe' and dist>120:
+            return 'Intermediate Rough'
+        elif cat=='Tee Box' and shot!=1:
+            return 'Fairway'
+        else:
+            return cat
 
-class SaveTheCats(object):
-	def __init__(self,epsilon):
-		self.epsilon = epsilon
-		if os.path.isfile('cats%g' % (epsilon,)):
-			pass
-		else:
-			os.makedirs('cats%g' % (epsilon,))
-			self.save_files()
+    def partition (lst, n):
+        return [lst[i::n] for i in xrange(n)]
 
-	def save_files(self):
-		self.data = pd.concat([pd.read_csv('data/%d.csv' % (year)) for year in range(2003,2017)])
-		self.data.columns = [col.replace('#','') for col in self.data.columns]
-		inds = {num:ind for ind,num in enumerate(pd.unique(data.Player_Number))}
-		data.insert(5,'Player_Index',[inds[num] for num in data.Player_Number])
-		self.n_players = len(inds)
-		rounds = data.groupby(['Tournament_Year','Permanent_Tournament_','Round_Number','Course_'])
-		# n_rounds = len(rounds)
-		for round_ind,df in enumerate(rounds):
-		    tup,df = df
-		    year,tournament,round,course = tup
-		    for cat in cats:
-		    	A,G = csc_matrix((n_players,n_players)),csc_matrix((n_players,n_players))
-		    	for condition in cats[cat]:
-		    		self.condition = 'Tournament_Year==@year & Permanent_Tournament_==@tournament & Round_Number==@round & Course_==@course & ' + condition 
-		    		A_,G_ = get_matrices()
-		    		A += A_
-		    		G += G_
-			    self.filename,self.array = '%s_%dA' % (cat,round_ind),A
-			    self.save_sparse_csc()
-			    self.filename,self.array = '%s_%dG' % (cat,round_ind),G
-			    self.save_sparse_csc()
-		    # if round_ind%300==0:
-		    #     print round_ind
+    def run_a_slice(slice):
+        def get_matrices(tup,conditon):
+            year,tournament,round,course,hole = tup
+            subset = data.query(condition)[['Started_at_X','Started_at_Y','Distance_from_hole','Strokes_Gained','Player_Index']].values
+            arr,arr1 = np.zeros((n_players,n_players)),np.zeros((n_players,n_players))
+            dists = squareform(pdist(subset[:,0:2]))
+            inds = [(i,j) for i,j in itertools.product(xrange(len(dists)),xrange(len(dists))) if i!=j and dists[i,j]<epsilon*subset[i,2] and dists[i,j]<epsilon*subset[j,2]]
+            for i,j in inds:
+                if arr1[i,j]!=0:
+                    continue
+                arr[int(subset[i,4]),int(subset[j,4])] += 1.0/(1.0 + math.exp(subset[i,3]-subset[j,3])) + .5
+                arr1[int(subset[i,4]),int(subset[j,4])] += 1.0
+            A,G = csc_matrix(arr),csc_matrix(arr1)
+            return A,G
 
-	def get_matrices(self):
-		subset = self.data.query(self.condition)[['Started_at_X','Started_at_Y','Distance_from_hole','Strokes_Gained','Player_Index']].values
-		arr,arr1 = np.zeros((self.n_players,self.n_players)),np.zeros((self.n_players,self.n_players))
-		dists = pdist(subset[:,0:2])
-		inds = [(i,j) for i,j in zip(xrange(len(subset)),xrange(len(subset))) if  
-											i!=j and dists[i,j]<self.epsilon*subset[i,2] and dists[i,j]<self.epsilon*subset[j,2]]
-		for i,j in inds:
-			arr[subset[i,4],subset[j,4]] += 1.0/(1.0 + math.exp(subset[i,3]-subset[j,3])) + .5
-			arr1[subset[i,4],subset[j,4]] += 1.0
-		A,G = csc_matrix(arr),csc_matrix(arr1)
-		return A,G
+        def save_sparse_csc(filename,array):
+            np.savez(filename,data=array.data,indices=array.indices,indptr=array.indptr,shape=array.shape)
+            return
 
-	def save_sparse_csc(self):
-    	np.savez(self.filename,data=self.array.data,indices=self.array.indices,indptr=self.array.indptr,shape=self.array.shape)
+        for ind,tup in slice:
+            for cat in cats:
+                condition = 'Year==@year & Permanent_Tournament_==@tournament & Round==@round & Course_==@course & Hole==@hole & ' + cats[cat] 
+                A,G = get_matrices(tuple(tup),condition)
+                save_sparse_csc('cats%g/%s_%dA' % (epsilon*100,cat,ind),A)
+                save_sparse_csc('cats%g/%s_%dG' % (epsilon*100,cat,ind),G)
+        return
 
+    _,epsilon = sys.argv
+    if os.path.isfile('cats%s' % (epsilon,)):
+        sys.exit('File already exists.')
+    else:
+        os.makedirs('cats%s' % (epsilon,))
+    epsilon = float(epsilon)/100
+    data = pd.concat([pd.read_csv('data/%d.csv' % (year)) for year in range(2003,2004)])
+    data.columns = [col.replace('#','') for col in data.columns]
+    inds = {num:ind for ind,num in enumerate(pd.unique(data.Player_))}
+    data.insert(5,'Player_Index',[inds[num] for num in data.Player_])
+    data.insert(len(data.columns),'Cat',[convert_cats(c,d,s) for c,d,s in zip(data['From_Location(Scorer)'],data['Distance_from_hole'],data.Shot)])
+    n_players = len(inds)
+    hole_tups = data[['Year','Permanent_Tournament_','Round','Course_','Hole']].drop_duplicates().reset_index().drop('index',axis=1).T.to_dict('list').items()
+    hole_tups = sorted(hole_tups)
+    print len(hole_tups)
+    with open('cats%g/key_file.csv' % (epsilon*100,),'w') as keyFile:
+        for tup in hole_tups:
+            keyFile.write(','.join(map(str,[tup[0]] + tup[1])) + '\n')
 
+    num_cores = multiprocessing.cpu_count()-1
+    slices = partition(hole_tups,num_cores)
+    pool = multiprocessing.Pool(num_cores)
+    results = pool.map(run_a_slice, slices)
+    pool.close()
 
-		
-		
