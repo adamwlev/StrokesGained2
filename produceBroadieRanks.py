7d2ee2289ac2acf738a1068aa609c3c81fbd8ded
adamwlev <awl44@me.com>
2016-09-18 13:34:23 -0400

    Recreating current ranks

--- produceBroadieRanks.py
+++ produceBroadieRanks.py
@@ -19,8 +19,6 @@ data = pd.concat([pd.read_csv('data/%d.csv' % year) for year in range(2003,2005)
 data.insert(len(data.columns),'Broadie_cat',[convert_broadie_cats(cat,dist,par) for cat,dist,par in zip(data.Cat,data.Distance_from_hole,data.Par_Value)])
 
 field_for_cat = data.groupby(['Year','Course_#','Round','Broadie_cat'])
-data.insert(len(data.columns),'SG_of_Field',[0]*len(data))
-d = field_for_cat.Strokes_Gained.count().to_dict()
-
-for key in sorted(d,key=d.get)[0:10]:
-	print key,d[key]
+d = field_for_cat.Strokes_Gained.mean().to_dict()
+data.insert(len(data.columns),'SG_of_Field',[d[tup] for tup in zip(data.Year,data['Course_#'],data.Round,data.Broadie_cat)])
+print data.SG_of_Field.describe()
