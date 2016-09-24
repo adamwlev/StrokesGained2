7ceea4eba30c4d79956125cd6cc186dc3413f476
adamwlev <awl44@me.com>
2016-09-23 22:08:13 -0700

    save the cats2

--- ProcessingPipeline/SaveTheCats2.py
+++ ProcessingPipeline/SaveTheCats2.py
@@ -132,7 +132,7 @@ if __name__=='__main__':
        tournament_groups[current_group].add(tuple(tup[0:2]))
        holes_to_inflate.append(tuple(tup))
 
-    num_cores = 25
+    num_cores = 36
     slices = partition(group_to_tups.items(),num_cores)
     pool = multiprocessing.Pool(num_cores)
     results = pool.map(run_a_slice, slices)
