0a5dc9d3b28fedb1edfe1c0031a77ee300568a40
adamwlev <awl44@me.com>
2016-09-24 21:46:30 -0700

    commits

--- ProcessingPipeline/SaveTheRanks.py
+++ ProcessingPipeline/SaveTheRanks.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 from scipy.sparse import csc_matrix,csr_matrix,eye,bmat
-from scipy.sparse.linalg import eigs,inv,gmres
+from scipy.sparse.linalg import eigs,inv,gmres,LinearOperator,spilu
 from scipy.stats import norm
 import pickle
 from collections import defaultdict
@@ -17,28 +17,46 @@ if __name__=="__main__":
 	def my_norm(x,BETA):
 	    return norm.pdf(x,0,BETA)/norm.pdf(0,0,BETA)
 
-	def alpha(A,a):
-	    A.data[A.data<1e-6] = 0
-	    A.data[np.isnan(A.data)]=0
-	    w,v = eigs(A,k=1,which='LM')
-	    return a/w[0].real
+	def run_a_slice(slice):
+		little_d = {}
 
-	def solve(mat,mat_1,a,min_reps,x_guess=None,x_guess1=None):
-	    mat.data[mat_1.data<1e-6] = 0
-	    mat_1.data[mat_1.data<1e-6] = 0
-	    mat.data[np.isnan(mat.data)] = 0
-	    mat_1.data[np.isnan(mat_1.data)] = 0
-	    
-	    S = eye(mat.shape[0],format='csc')-alpha(mat,a)*mat
-	    w_a = gmres(S,mat.sum(1),x0=x_guess)[0]
-	    
-	    S = eye(mat_1.shape[0],format='csc')-alpha(mat_1,a)*mat_1 
-	    w_g = gmres(S,mat_1.sum(1),x0=x_guess1)[0]
-	    
-	    solve.w_a = w_a
-	    solve.w_g = w_g
-	    w_a[w_g<min_reps] = 0
-	    return ((w_a/w_g)[-n_players:],w_g[-n_players:])
+		def alpha(A,a):
+		    A.data[A.data<1e-6] = 0
+		    A.data[np.isnan(A.data)]=0
+		    w,v = eigs(A,k=1,which='LM')
+		    return a/w[0].real
+
+		def solve(mat,mat_1,a,min_reps,x_guess=None,x_guess1=None):
+		    mat.data[mat_1.data<1e-6] = 0
+		    mat_1.data[mat_1.data<1e-6] = 0
+		    mat.data[np.isnan(mat.data)] = 0
+		    mat_1.data[np.isnan(mat_1.data)] = 0
+		    
+		    S = eye(mat.shape[0],format='csc')-alpha(mat,a)*mat
+		    M = spilu(S)
+		    M_x = lambda x: M.solve(x)
+		    M = LinearOperator(S.shape, M_x)
+		    w_a = gmres(S,mat.sum(1),x0=x_guess,M=M)[0]
+		    
+		    S = eye(mat_1.shape[0],format='csc')-alpha(mat_1,a)*mat_1 
+		    M = spilu(S)
+		    M_x = lambda x: M.solve(x)
+		    M = LinearOperator(S.shape, M_x)
+		    w_g = gmres(S,mat_1.sum(1),x0=x_guess1,M=M)[0]
+		    
+		    solve.w_a = w_a
+		    solve.w_g = w_g
+		    w_a[w_g<min_reps] = 0
+		    return ((w_a/w_g)[-n_players:],w_g[-n_players:])
+
+		for group in slice:
+		    min_ = max(0,group-window_size)*n_players
+		    max_ = group*n_players
+		    A_,G_ = A[min_:max_,min_:max_],G[min_:max_,min_:max_]
+	        res = solve(A_,G_,a,1)
+	        little_d[group] = res
+
+	    return little_d
 
 	def load_sparse_csc(filename):
 	    loader = np.load(filename)
@@ -69,20 +87,16 @@ if __name__=="__main__":
 		ranks,reps = [],[]
 		A = bmat([[bmat([[load_sparse_csc('./../cats/cats_w%s-%s-%s-%s/%s_%d.npz' % (epsilon,e_d,e_t,w_d,cat,group)) * my_norm(abs(i-group),beta)] for i in range(1,n_tournament_groups)]) for group in range(1,n_tournament_groups)]],format='csc')
 		G = bmat([[bmat([[load_sparse_csc('./../cats/cats_w%s-%s-%s-%s/%s_%d_g.npz' % (epsilon,e_d,e_t,w_d,cat,group)) * my_norm(abs(i-group),beta)] for i in range(1,n_tournament_groups)]) for group in range(1,n_tournament_groups)]],format='csc')
-		for group in range(1,n_tournament_groups):
-		    min_ = max(0,group-window_size)*n_players
-		    max_ = group*n_players
-		    A_,G_ = A[min_:max_,min_:max_],G[min_:max_,min_:max_]
-		    if group==1:
-		        res = solve(A_,G_,a,1)
-		        ranks.append(res[0])
-		        reps.append(res[1])
-		    else:
-		        w_a_approx = np.append(solve.w_a[0 if group<=window_size else n_players:],solve.w_a[-n_players:])
-		        w_g_approx = np.append(solve.w_g[0 if group<=window_size else n_players:],solve.w_g[-n_players:])
-		        res = solve(A_,G_,a,1,w_a_approx,w_g_approx)
-		        ranks.append(res[0])
-		        reps.append(res[1])
+		
+		num_cores = 10
+		slices = partition(range(1,n_tournament_groups),num_cores)
+		pool = multiprocessing.Pool(num_cores)
+		results = pool.map(run_a_slice, slices)
+		pool.close()
+
+		ranks = [results[i][0] for i in range(1,n_tournament_groups)]
+		reps = [results[i][1] for i in range(1,n_tournament_groups)]
+
 		np.save('./../ranks/ranks-%s-%s-%s-%s-%g-%g/%s_ranks.npy' % (epsilon,e_d,e_t,w_d,a,beta,cat), np.array(ranks).T)
 		np.save('./../ranks/ranks-%s-%s-%s-%s-%g-%g/%s_reps.npy' % (epsilon,e_d,e_t,w_d,a,beta,cat), np.array(reps).T)
 
