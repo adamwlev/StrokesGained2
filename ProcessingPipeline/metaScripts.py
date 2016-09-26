54580c317144c8b90c609e73f54fbbbf3deb5447
adamwlev <awl44@me.com>
2016-09-26 08:24:30 -0700

    m

--- ProcessingPipeline/metaScripts.py
+++ ProcessingPipeline/metaScripts.py
@@ -7,7 +7,7 @@ if __name__=="__main__":
 	cats = ['tee3','tee45','green0','green5','green10','green20','rough0','rough90',
 			'rough375','fairway0','fairway300','fairway540','bunker','other']
 	
-	for a in ['0.7','0.8','0.9']:
-		for beta in ['8','10','12']:
+	for a in ['0.83','0.93']:
+		for beta in ['10','12','14']:
 			for cat in cats:
 				subprocess.call(["python","SaveTheRanks.py" ,cat,"%s" % epsilon,"%s" % e_d,"%s" % e_t,"%s" % w_d, "%s" % a, "%s" % beta])
