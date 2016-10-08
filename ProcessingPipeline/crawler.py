7ff246c9bdfd42d21b923868ac3df7e7099123e5
adamwlev <awl44@me.com>
2016-10-07 19:10:16 -0700

    mm

--- ProcessingPipeline/crawler.py
+++ ProcessingPipeline/crawler.py
@@ -34,7 +34,7 @@ if __name__=="__main__":
 		for cat in cats:
 			if not os.path.exists('./../ranks/ranks_%s-%s-%s-%s-%s-%s/%s_ranks.npy' % (eps_,e_d_,e_t_,w_d_,alpha_,beta_,cat)): 
 				subprocess.call(["python","SaveTheRanks.py" ,"%s" % cat,"%s" % eps_,"%s" % e_d_,"%s" % e_t_,"%s" % w_d_, "%s" % alpha_, "%s" % beta_])
-				subprocess.call(["rsync","-avL","--progress","-e","ssh","-i","/home/ubuntu/.ssh/aws_ds8key.pem",
+				subprocess.call(["rsync","-avL","--progress","-e",'"ssh',"-i",'/home/ubuntu/.ssh/aws_ds8key.pem"',
 								 "/home/ubuntu/project/Rank_a_Golfer/ranks/ranks_%s-%s-%s-%s-%s-%s" % (eps_,e_d_,e_t_,w_d_,alpha_,beta_),
 								 "ubuntu@ec2-52-23-248-152.compute-1.amazonaws.com:~/project/Rank_a_Golfer/ranks/"])
 		
