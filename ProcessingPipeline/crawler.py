cfe40242342fa0a7f1f4baec51196bd38b0820e6
adamwlev <awl44@me.com>
2016-10-11 12:08:34 -0700

    Update Crawlers

--- ProcessingPipeline/crawler.py
+++ ProcessingPipeline/crawler.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import os
 import itertools
+import commands
 
 if __name__=="__main__":
 	
@@ -15,30 +16,19 @@ if __name__=="__main__":
 	cats = ['tee3','tee45','green0','green5','green10','green20','rough0','rough90',
 			'rough375','fairway0','fairway300','fairway540','bunker','other']
 
-	done_cats,done_ranks = set(),set()
-	if not os.path.exists('outFile.csv'):
-		outFile = open('outFile.csv','w')
-	else:
-		with open('outFile.csv','r') as f:
-			for line in f.read().splitlines():
-				if line.strip().split('-')[-1]=="cats":
-					done_cats.add(tuple(map(float,line.strip().split('-')[:-1])))
-				else:
-					done_ranks.add(tuple(map(float,line.strip().split('-')[:-1])))
-		outFile = open('outFile.csv','a')
-
 	for eps_,e_d_,e_t_,w_d_,alpha_,beta_ in itertools.product(*[eps,e_d,e_t,w_d,alpha,beta]):
-		if tuple(map(float,[eps_,e_d_,e_t_,w_d_])) not in done_cats:
+		if not os.path.exists('./../cats/cats_w%s-%s-%s-%s' % (eps_,e_d_,e_t_,w_d_)):
+			print 'Running SaveTheCats %s-%s-%s-%s' % (eps_,e_d_,e_t_,w_d_)
 			subprocess.call(["python","SaveTheCats.py","%s" % eps_,"%s" % e_d_,"%s" % e_t_,"%s" % w_d_])
-			outFile.write("%s-%s-%s-%s-cats" % (eps_,e_d_,e_t_,w_d_))
-
-		if tuple(map(float,[eps_,e_d_,e_t_,w_d_,alpha_,beta_])) not in done_ranks:
-			for cat in cats:
-				if not os.path.exists('./../ranks/ranks-%s-%s-%s-%s-%s-%s/%s_ranks.npy' % (eps_,e_d_,e_t_,w_d_,alpha_,beta_,cat)): 
-					subprocess.call(["python","SaveTheRanks.py" ,"%s" % cat,"%s" % eps_,"%s" % e_d_,"%s" % e_t_,"%s" % w_d_, "%s" % alpha_, "%s" % beta_])
-					cmd = "rsync -avL --progress -e \"ssh -i /home/ubuntu/aws_ds8key.pem\" /home/ubuntu/project/Rank_a_Golfer/ranks/ranks-%s-%s-%s-%s-%s-%s ubuntu@ec2-52-23-248-152.compute-1.amazonaws.com:~/project/Rank_a_Golfer/ranks/" % (eps_,e_d_,e_t_,w_d_,alpha_,beta_)
-					os.system(cmd)
-			outFile.write("%s-%s-%s-%s-%s-%s-ranks" % (eps_,e_d_,e_t_,w_d_,alpha_,beta_))
-			outFile.write("\n")
-
-	    	
+		else:
+			n_files = len(commands.getstatusoutput('find ./../cats/cats_w%s-%s-%s-%s -type f' % (eps_,e_d_,e_t_,w_d_))[1].split('\n'))
+			print n_files
+			if n_files!=3808:
+				print 'Running SavetheCats %s-%s-%s-%s' % (eps_,e_d_,e_t_,w_d_)
+				subprocess.call(["python","SaveTheCats.py","%s" % eps_,"%s" % e_d_,"%s" % e_t_,"%s" % w_d_])
+		for cat in cats:
+			if not os.path.exists('./../ranks/ranks-%s-%s-%s-%s-%s-%s/%s_ranks.npy' % (eps_,e_d_,e_t_,w_d_,alpha_,beta_,cat)): 
+				print 'Running SaveTheRanks %s-%s-%s-%s-%s-%s %s' % (eps_,e_d_,e_t_,w_d_,alpha_,beta_,cat)
+				subprocess.call(["python","SaveTheRanks.py" ,"%s" % cat,"%s" % eps_,"%s" % e_d_,"%s" % e_t_,"%s" % w_d_, "%s" % alpha_, "%s" % beta_])
+				cmd = "rsync -avL --progress -e \"ssh -i /home/ubuntu/aws_ds8key.pem\" /home/ubuntu/project/Rank_a_Golfer/ranks/ranks-%s-%s-%s-%s-%s-%s ubuntu@ec2-52-23-248-152.compute-1.amazonaws.com:~/project/Rank_a_Golfer/ranks/" % (eps_,e_d_,e_t_,w_d_,alpha_,beta_)
+				os.system(cmd)
--- ProcessingPipeline/meta_crawler.py
+++ ProcessingPipeline/meta_crawler.py
@@ -3,8 +3,8 @@ import sys
 import os
 
 if __name__=="__main__":
-	cmd = "rsync -avL --progress -e \"ssh -i /home/ubuntu/aws_ds8key.pem\" ubuntu@ec2-52-23-248-152.compute-1.amazonaws.com:~/project/Rank_a_Golfer/ranks/ /home/ubuntu/project/Rank_a_Golfer/ranks"
+	cmd = "rsync -avL --progress -e \"ssh -i /home/ubuntu/aws_ds8key.pem\" ubuntu@ec2-52-23-248-152.compute-1.amazonaws.com:~/project/Rank_a_Golfer/ranks/ /home/ubuntu/project/Rank_a_Golfer/ranks/"
 	os.system(cmd)
-	cmd = "rsync -avL --progress -e \"ssh -i /home/ubuntu/aws_ds8key.pem\" ubuntu@ec2-52-23-248-152.compute-1.amazonaws.com:~/project/Rank_a_Golfer/cats/ /home/ubuntu/project/Rank_a_Golfer/cats"
+	cmd = "rsync -avL --progress -e \"ssh -i /home/ubuntu/aws_ds8key.pem\" ubuntu@ec2-52-23-248-152.compute-1.amazonaws.com:~/project/Rank_a_Golfer/cats/ /home/ubuntu/project/Rank_a_Golfer/cats/"
 	os.system(cmd)
 	subprocess.call(["python","crawler.py"])
