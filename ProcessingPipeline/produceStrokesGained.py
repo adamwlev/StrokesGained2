3e98b18e09f9d6bccc9fa1f2cbba06f3fc1d8fa4
adamwlev <awl44@me.com>
2016-11-10 21:32:10 -0800

    new file

--- ProcessingPipeline/produceStrokesGained.py
+++ ProcessingPipeline/produceStrokesGained.py
@@ -59,8 +59,8 @@ for cat in cats[4:]:
 	    predictions = bst.predict(dtest,ntree_limit=bst.best_iteration)
 	    error = np.mean((predictions-y[test])**2)
 	    print '***FOLD %d *** ERROR %g ***' % (u,error)
-	    print y[test].shape,data.iloc[data_.index[test]]['Shots_taken_from_location'].values.shape
-	    assert np.all(y[test],data.loc[data_.index[test],'Shots_taken_from_location'].values)
+
+	    assert np.all(y[test],data.iloc[data_.index[test]]['Shots_taken_from_location'].values)
 	    results.update({ind:pred for ind,pred in zip(data_.index[test],predictions)})
 
 	print len(results),len(data_)
