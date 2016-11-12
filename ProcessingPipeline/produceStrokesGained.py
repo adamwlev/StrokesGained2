24b158241af89d76bad04c860aec1965ed4cedff
adamwlev <awl44@me.com>
2016-11-12 14:41:18 -0800

    updating produceStrokesGained.py

---
+++ ProcessingPipeline/produceStrokesGained.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+for year in range(2003,2017):
+	data = pd.read_csv('./../data/%d.csv' % (year))
+	score_min,score_max = data.Hole_Score.min(),data.Hole_Score.max()
+	data.insert(len(data.columns),'Strokes_Gained',[0]*len(data))
+	data = data.sort_values(['Year','Player_#','Course_#','Round','Hole'])
+	for i in range(score_min,score_max+1):
+		for j in range(1,i+1):
+			data.loc[(data.Hole_Score==i) & (data.Shot==j),'Strokes_Gained'] = data.loc[(data.Hole_Score==1) & (data.Shot==j),'Difficulty_Start'] - \
+																			   data.loc[(data.Hole_Score==1) & (data.Shot==j+1),'Difficulty_Start'] - 1
+	data.to_csv('./../data/%d.csv',index=False)
