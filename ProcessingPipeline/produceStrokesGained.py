f22d1f0780b81b65a8a3d715eb8ecfcb3f68d865
adamwlev <awl44@me.com>
2016-11-12 15:19:55 -0800

    updates

--- ProcessingPipeline/produceStrokesGained.py
+++ ProcessingPipeline/produceStrokesGained.py
@@ -9,6 +9,9 @@ for year in range(2003,2017):
 	data = data.sort_values(['Year','Player_#','Course_#','Round','Hole'])
 	for i in range(score_min,score_max+1):
 		for j in range(1,i+1):
-			data.loc[(data.Hole_Score==i) & (data.Shot==j),'Strokes_Gained'] = data.loc[(data.Hole_Score==1) & (data.Shot==j),'Difficulty_Start'] - \
-																			   data.loc[(data.Hole_Score==1) & (data.Shot==j+1),'Difficulty_Start'] - 1
+			if i==j:
+				data.loc[(data.Hole_Score==i) & (data.Shot==j),'Strokes_Gained'] = data.loc[(data.Hole_Score==1) & (data.Shot==j),'Difficulty_Start'] - 1
+			else:
+				data.loc[(data.Hole_Score==i) & (data.Shot==j),'Strokes_Gained'] = data.loc[(data.Hole_Score==1) & (data.Shot==j),'Difficulty_Start'] - \
+																			       data.loc[(data.Hole_Score==1) & (data.Shot==j+1),'Difficulty_Start'] - 1
 	data.to_csv('./../data/%d.csv' % year,index=False)
