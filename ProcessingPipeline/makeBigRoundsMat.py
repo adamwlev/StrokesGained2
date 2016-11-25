77dde3127e7611f79c0e28181b0271d83a0cc946
adamwlev <awl44@me.com>
2016-11-25 13:45:58 -0800

    updates

--- ProcessingPipeline/makeBigRoundsMat.py
+++ ProcessingPipeline/makeBigRoundsMat.py
@@ -2,6 +2,7 @@ def return_mats(BETA):
 	from scipy.sparse import bmat,csc_matrix
 	from scipy.stats import norm
 	import numpy as np
+	import pandas as pd
 
 	def load_sparse_csc(filename):
 	    loader = np.load(filename)
@@ -10,7 +11,11 @@ def return_mats(BETA):
 	def my_norm(x,BETA):
 	    return norm.pdf(x,0,BETA)/norm.pdf(0,0,BETA)
 
-	n_tournament_groups = 156
+	data = pd.read_csv('./../data/round.csv')
+	tups = data.drop_duplicates(['Tournament_Year','Permanent_Tournament_#'])[['Tournament_Year','Permanent_Tournament_#']].values.tolist()
+	tournament_groups = {tuple(tup):u/4 for u,tup in enumerate(tups)}
+	data.insert(len(data.columns),'Tournament_Group',[tournament_groups[tuple(tup)] for tup in data[['Tournament_Year','Permanent_Tournament_#']].values.tolist()])
+	n_tournament_groups = len(pd.unique(data.Tournament_Group))
 
 	A = bmat([[bmat([[load_sparse_csc('./../rounds/%dA.npz' % i)*my_norm(i-k,BETA)] 
                  for k in range(0,n_tournament_groups)],format='csc') 
