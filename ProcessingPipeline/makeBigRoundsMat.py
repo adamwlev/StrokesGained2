5ed658a486b1cb59c8393b920ebd8bc2adf360b9
adamwlev <awl44@me.com>
2016-11-25 11:28:31 -0800

    updates

--- ProcessingPipeline/makeBigRoundsMat.py
+++ ProcessingPipeline/makeBigRoundsMat.py
@@ -2,6 +2,10 @@ def return_mats(beta):
 	from scipy.sparse import bmat
 	from scipy.stats import norm
 
+	def load_sparse_csc(filename):
+	    loader = np.load(filename)
+	    return csc_matrix((loader['data'],loader['indices'],loader['indptr']),shape = loader['shape'])
+
 	def my_norm(x,BETA):
 	    return norm.pdf(x,0,BETA)/norm.pdf(0,0,BETA)
 
