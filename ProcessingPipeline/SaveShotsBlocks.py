29381bbda8014e7d4b90a7cdf809abce444cc8db
adamwlev <awl44@me.com>
2016-11-26 17:02:41 -0800

    new save blocks

--- ProcessingPipeline/SaveShotsBlocks.py
+++ ProcessingPipeline/SaveShotsBlocks.py
@@ -54,11 +54,10 @@ if __name__=='__main__':
         return [lst[i::n] for i in xrange(n)]
 
     def run_a_slice(slice):
-        def get_matrix(tups,conditon):
+        def get_matrix(tournament_group,conditon):
             arr,arr1 = np.zeros((n_players,n_players)),np.zeros((n_players,n_players))
-            for tup in tups:
-                year,tournament,round,course,hole = tup
-                subset = data.query(condition)[['Started_at_X','Started_at_Y','Distance_from_hole','Strokes_Gained','Time','Player_Index']].values
+            for (round,course,hole),df in data[data.Tournament_Group==tournament_group].groupby(['Round','Course_','Hole']):
+                subset = df.query(condition)[['Started_at_X','Started_at_Y','Distance_from_hole','Strokes_Gained','Time','Player_Index']].values
                 dists = squareform(pdist(subset[:,0:2]))
                 inds = [(i,j) for i,j in itertools.product(xrange(len(dists)),xrange(len(dists))) if i!=j and dists[i,j]<epsilon*subset[i,2] and dists[i,j]<epsilon*subset[j,2]]
                 for i,j in inds:
@@ -74,25 +73,25 @@ if __name__=='__main__':
             np.savez(filename,data=array.data,indices=array.indices,indptr=array.indptr,shape=array.shape)
             return
 
-        for group,tups in slice:
+        for tournament_group in slice:
             for big_cat in meta_cats:
                 if os.path.exists('./../cats/cats_w%g-%g-%g-%g/%s_%d.npz' % (epsilon*100,e_d,e_t,w_d,big_cat,group)):
                     continue
                 mat,mat1 = None,None
                 for small_cat in meta_cats[big_cat]:
-                    condition = 'Year==@year & Permanent_Tournament_==@tournament & Round==@round & Course_==@course & Hole==@hole & ' + cats[small_cat] 
+                    condition = cats[small_cat] 
                     try:
                         mat.data
                     except:
-                        mat,mat1 = get_matrix(tups,condition)
+                        mat,mat1 = get_matrix(tournament_group,condition)
                         gc.collect()
                     else:
-                        res = get_matrix(tups,condition)
+                        res = get_matrix(tournament_group,condition)
                         gc.collect()
                         mat += res[0]
                         mat1 += res[1]
-                save_sparse_csc('./../cats/cats_w%g-%g-%g-%g/%s_%d' % (epsilon*100,e_d,e_t,w_d,big_cat,group),mat)
-                save_sparse_csc('./../cats/cats_w%g-%g-%g-%g/%s_%d_g' % (epsilon*100,e_d,e_t,w_d,big_cat,group),mat1)
+                save_sparse_csc('./../cats/cats_w%g-%g-%g-%g/%s_%d' % (epsilon*100,e_d,e_t,w_d,big_cat,tournament_group),mat)
+                save_sparse_csc('./../cats/cats_w%g-%g-%g-%g/%s_%d_g' % (epsilon*100,e_d,e_t,w_d,big_cat,tournament_group),mat1)
                 cmd = "rsync -avL --progress -e \"ssh -i /home/ubuntu/aws_ds8key.pem\" /home/ubuntu/project/Rank_a_Golfer/cats/cats_w%g-%g-%g-%g ubuntu@ec2-54-162-31-22.compute-1.amazonaws.com:~/project/Rank_a_Golfer/cats/" % (epsilon*100,e_d,e_t,w_d)
                 os.system(cmd)
         return
@@ -104,38 +103,26 @@ if __name__=='__main__':
     e_d,e_t,w_d = tuple(map(float,[e_d,e_t,w_d]))
     data = pd.concat([pd.read_csv('./../data/%d.csv' % (year)) for year in range(2003,2017)])
     data.columns = [col.replace('#','') for col in data.columns]
-    inds = {num:ind for ind,num in enumerate(pd.unique(data.Player_))}
-    data.insert(5,'Player_Index',[inds[num] for num in data.Player_])
+    
+    with open('./../PickleFiles/num_to_ind_shot.pkl.pkl','r') as pickleFile:
+        num_to_ind = pickle.load(pickleFile)
+
+    with open('./../PickleFiles/tourn_order.pkl','r') as pickleFile:
+        tourn_order = pickle.load(pickleFile)
+
+    data.insert(5,'Player_Index',[num_to_ind[num] for num in data.Player_])
+    n_players = len(pd.unique(data.Player_Index))
     data.Time = data.Time.values/100 * 60 + data.Time.values%100
     data = data[['Cat','Year','Round','Permanent_Tournament_','Course_','Hole','Started_at_X','Started_at_Y','Distance_from_hole','Strokes_Gained','Time','Player_Index','Par_Value']]
     gc.collect()
-
-    with open('./../hole_tups.pkl','r') as pickleFile:
-        hole_tups = pickle.load(pickleFile)
-    n_players = len(inds)
-    n_holes = len(hole_tups)
-    n_tournaments = len(pd.DataFrame(np.array(hole_tups))[[0,1]].drop_duplicates())
-
-    bin_size = 4
+    
+    tournament_groups = {tup:u/4 for u,tup in enumerate(tourn_order)}
+    data.insert(len(data.columns),'Tournament_Group',[tournament_groups[tuple(tup)] for tup in data[['Year','Permanent_Tournament_']].values.tolist()])
+    n_tournament_groups = len(pd.unique(data.Tournament_Group))
     window_size = 28
-    n_tournament_groups = int(math.ceil(n_tournaments/float(bin_size)))
-    current_group = 0
-    tournament_groups = defaultdict(set)
-    tournaments = set()
-    group_to_tups = {}
-    holes_to_inflate = []
-    for tup in hole_tups:
-       tournaments.add(tuple(tup[0:2]))
-       tournament_group = (len(tournaments)-1)/bin_size
-       if tournament_group>current_group:
-           current_group = tournament_group
-           group_to_tups[current_group] = holes_to_inflate
-           holes_to_inflate = []
-       tournament_groups[current_group].add(tuple(tup[0:2]))
-       holes_to_inflate.append(tuple(tup))
 
-    num_cores = multiprocessing.cpu_count()-1
-    slices = partition(group_to_tups.items(),num_cores)
+    num_cores = 8
+    slices = partition(range(n_tournament_groups),num_cores)
     pool = multiprocessing.Pool(num_cores)
     results = pool.map(run_a_slice, slices)
     pool.close()
