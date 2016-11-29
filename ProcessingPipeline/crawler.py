e8c91ffb0b00d6b637fd1fbc86c6ba6f436311ff
adamwlev <awl44@me.com>
2016-11-28 21:58:47 -0800

    updates

--- ProcessingPipeline/crawler.py
+++ ProcessingPipeline/crawler.py
@@ -8,8 +8,8 @@ import multiprocessing
 if __name__=="__main__":
 	
 	eps = ['300']
-	e_d = ['0.23','0.66']
 	e_t = ['0.15','0.4']
+	e_d = ['0.23','0.66']
 	w_d = ['0.7','0.95']
 	alpha = ['0.9','0.99']
 	beta = ['5','11','15']
@@ -19,7 +19,7 @@ if __name__=="__main__":
 
 	def run_a_slice(cats):
 		for cat in cats:
-			if not os.path.exists('./../ranks/ranks-%s-%s-%s-%s-%s-%s/%s_ranks.npy' % (eps_,e_d_,e_t_,w_d_,alpha_,beta_,cat)): 
+			if not os.path.exists('./../ranks/ranks-%s-%s-%s-%s-%s-%s/%s_ranks.npy' % (eps_,e_t_,e_d_,w_d_,alpha_,beta_,cat)): 
 				print 'Running produceRanks %s-%s-%s-%s-%s-%s %s' % (eps_,e_d_,e_t_,w_d_,alpha_,beta_,cat)
 				subprocess.call(["python","produceRanks.py" ,"%s" % cat,"%s" % eps_,"%s" % e_d_,"%s" % e_t_,"%s" % w_d_, "%s" % alpha_, "%s" % beta_])
 				cmd = "rsync -avL --progress -e \"ssh -i /home/ubuntu/aws_ds8key.pem\" /home/ubuntu/project/Rank_a_Golfer/ranks/ranks-%s-%s-%s-%s-%s-%s ubuntu@ec2-54-162-31-22.compute-1.amazonaws.com:~/project/Rank_a_Golfer/ranks/" % (eps_,e_d_,e_t_,w_d_,alpha_,beta_)
